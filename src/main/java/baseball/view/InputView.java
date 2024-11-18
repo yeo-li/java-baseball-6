85bcab852c29f65e5db46d71e6c12389e271df5a
yeo-li <81564503+yeo-li@users.noreply.github.com>
2024-11-19 00:39:50 +0900

    feat(BaseballController, InputView): 사용자 숫자 입력 안내 메세지 출력 및 입력 기능 구현

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,13 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String inputNumbers() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String userInput = Console.readLine();
+
+        return userInput;
+    }
+}
