4fb60e024b9ccdefe634d2090247d4d42e971852
yeo-li <81564503+yeo-li@users.noreply.github.com>
2024-11-19 00:29:34 +0900

    feat(BaseballController): 숫자 야구 정답 생성 기능 구현

---
+++ src/main/java/baseball/controller/BaseballController.java
@@ -0,0 +1,28 @@
+package baseball.controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballController {
+
+    public void startBaseballGame() {
+        List<Integer> answer = createRandomAnswer();
+
+
+    }
+
+
+    public List<Integer> createRandomAnswer() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
+    }
+
+
+}
