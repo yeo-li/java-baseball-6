682fc9b827136c04e670e7164d627e06e90b5bcd
yeo-li <81564503+yeo-li@users.noreply.github.com>
2024-11-19 15:46:20 +0900

    feat(BaseballController): 스트라이크 및 볼 갯수 계산 기능 구현

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,17 +1,33 @@
 package baseball.controller;
 
+import baseball.util.StringParser;
+import baseball.validator.NumberValidator;
+import baseball.view.InputView;
+import baseball.view.OutputView;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballController {
 
-    public void startBaseballGame() {
-        List<Integer> answer = createRandomAnswer();
+    private final InputView inputView;
+    private final OutputView outputView;
 
-
+    public BaseballController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
     }
 
+    public void startBaseballGame() {
+        outputView.printGameStartMessage();
+        List<Integer> answer = createRandomAnswer();
+
+        String userInput = inputNumbers();
+        List<Integer> numbers = StringParser.parse(userInput);
+
+        int strike = calculateStrike(answer, numbers);
+        int ball = calculateBall(answer, numbers);
+    }
 
     public List<Integer> createRandomAnswer() {
         List<Integer> numbers = new ArrayList<>();
@@ -24,5 +40,34 @@ public class BaseballController {
         return numbers;
     }
 
+    public String inputNumbers() {
+        String userInput = inputView.inputNumbers();
+        NumberValidator.validate(userInput);
+        return userInput;
+    }
 
+    public int calculateStrike(List<Integer> answer, List<Integer> numbers) {
+        int strike = 0;
+        for (int i = 0; i < 3; i++) {
+            if (answer.get(i) == numbers.get(i)) {
+                strike++;
+            }
+        }
+        return strike;
+    }
+
+    public int calculateBall(List<Integer> answer, List<Integer> numbers) {
+        int ball = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (answer.get(i) == numbers.get(j)) {
+                    ball++;
+                }
+            }
+        }
+        return ball;
+    }
 }
